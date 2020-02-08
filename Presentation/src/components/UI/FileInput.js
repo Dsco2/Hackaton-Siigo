4809d4724c0cf7fc7c634523b3f6779ff2ce04a2
Felipe Suárez <suarez.a.felipe@gmail.com>
2020-02-08 17:05:25 -0500

    Add upload file form

--- Presentation/src/components/UI/FileInput.js
+++ Presentation/src/components/UI/FileInput.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FileInput extends Component {
-    render() {
-        return (
-            <div>
-                 <h4>hello world from file input</h4>
-            </div>
-        );
-    }
-}
+const FileInput = props => {
+    return (
+        <form>
+            <input type='file' name='file' className='btn btn-secondary' onChange={props.onChange} />
+            {props.loading ? (
+                <button className='btn btn-primary pull-right' type='button' disabled>
+                    <span className='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>
+                    &nbsp;&nbsp;Cargando...
+                </button>
+            ) : (
+                <button type='submit' className='btn btn-primary' onClick={props.onUpload}>
+                    Subir archivo CSV
+                </button>
+            )}
+        </form>
+    );
+};
 
 export default FileInput;
--- Presentation/src/containers/Products/Products.js
+++ Presentation/src/containers/Products/Products.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import TenantNavbar from "../../components/Navbar/TenantNavbar";
+import FileInput from "../../components/UI/FileInput";
+import axios from "axios";
 
 class Products extends Component {
+  state = {
+    file: {}
+  };
+
+  fileHandler = event => {
+    console.log(event.target.files[0]);
+    this.setState({ file: event.target.files[0] });
+  };
+
+  onUploadHandler = event => {
+    event.preventDefault();
+    debugger;
+    const data = {
+      idTenant: 3,
+      file: this.state.file
+    };
+    axios
+      .post("products/upload-file", data)
+      .then(response => response)
+      .catch(error => error);
+  };
+
   render() {
     return (
       <>
         <TenantNavbar />
         <h1>hello world from Products</h1>
+
+        <FileInput
+          onChange={this.fileHandler}
+          onUpload={this.onUploadHandler}
+          loading={false}
+        />
       </>
     );
   }
