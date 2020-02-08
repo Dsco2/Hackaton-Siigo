2de0eaca9218120a470640d1d0e49823669b9964
Felipe Suárez <suarez.a.felipe@gmail.com>
2020-02-08 09:27:05 -0500

    Initial architecture and DB creation

---
+++ Persistence/MainContext.cs
@@ -0,0 +1,23 @@
+﻿using Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Maps;
+
+
+namespace Persistence
+{
+    public class MainContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Hackathon");
+
+        public DbSet<Customer> Data { get; set; }
+
+        public MainContext(DbContextOptions<MainContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Persistence/Maps/CustomerMap.cs
@@ -0,0 +1,16 @@
+﻿
+using Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Maps
+{
+    public class CustomerMap: IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("customer");
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
