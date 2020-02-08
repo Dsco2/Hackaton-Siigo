174c06496053bd4a0cf56a83db0ab5b47532fd51
Felipe Suárez <suarez.a.felipe@gmail.com>
2020-02-08 11:28:26 -0500

    Add presentation layer

---
+++ Presentation/src/containers/Search/SearchProducts.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SearchProducts extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hello world from search products</h3>
+      </div>
+    );
+  }
+}
+
+export default SearchProducts;
---
+++ Presentation/src/containers/UploadData/UplaodData.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UplaodData extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello world from upload data</h1>
+      </div>
+    );
+  }
+}
+
+export default UplaodData;
