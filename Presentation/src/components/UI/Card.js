40f288f9b0c28f21bb58e002b8969260af758128
Felipe Suárez <suarez.a.felipe@gmail.com>
2020-02-09 12:25:56 -0500

    Fix console warnings in presentation layer

--- Presentation/src/components/UI/Card.js
+++ Presentation/src/components/UI/Card.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Card = ({ header, title, children }) => {
   return (
-    <div class="card">
-      <h5 class="card-header">{header}</h5>
-      <div class="card-body">
-        <h5 class="card-title">{title}</h5>
-        <div class="card-text">{children}</div>
+    <div className="card">
+      <h5 className="card-header">{header}</h5>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <div className="card-text">{children}</div>
       </div>
     </div>
   );
